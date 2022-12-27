0772f36b91589b21eb00cc9622d3f28fb529abd7
Linecom <69106177+EBT01@users.noreply.github.com>
2022-12-27 09:22:13 +0800

    Add i18n English

---
+++ en/function.js
@@ -0,0 +1,7 @@
+function vSec() {
+    var x = document.forms["s"]["wd"].value;
+        if (x == null || x == "") {
+            alert("Keywords cannot be empty!");
+            return false;
+        }
+    }
